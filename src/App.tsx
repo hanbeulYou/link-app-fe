cc4d363cffd25a7e60da212ffd909237fab08be0
hanbeulYou <hanbeul.you@gmail.com>
2024-11-23 22:54:01 +0900

    feat: 페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 );
 
