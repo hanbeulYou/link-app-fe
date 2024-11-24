066d6b23b1f8a79879025d1cf167c4b9dc86a95c
hanbeulYou <hanbeul.you@gmail.com>
2024-11-24 14:56:53 +0900

    chore: 웹뷰 확인용 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,48 +9,51 @@ import About from "./pages/About";
 import { AuthLayout, DefaultLayout } from "./components/layout/PageLayout";
 
 const App: React.FC = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <DefaultLayout>
-              <Home />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <DefaultLayout>
-              <About />
-            </DefaultLayout>
-          }
-        />
+  <div style={{ width: "100%", height: "100%", backgroundColor: "aqua" }}>
+    <h1>Hello World</h1>
+  </div>
+  //   <ThemeProvider theme={theme}>
+  //     <GlobalStyle />
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route
+  //           path="/"
+  //           element={
+  //             <DefaultLayout>
+  //               <Home />
+  //             </DefaultLayout>
+  //           }
+  //         />
+  //         <Route
+  //           path="/about"
+  //           element={
+  //             <DefaultLayout>
+  //               <About />
+  //             </DefaultLayout>
+  //           }
+  //         />
 
-        {/* Auth Layout 적용 */}
-        <Route
-          path="/login"
-          element={
-            <AuthLayout>
-              <div>Login</div>
-            </AuthLayout>
-          }
-        />
+  //         {/* Auth Layout 적용 */}
+  //         <Route
+  //           path="/login"
+  //           element={
+  //             <AuthLayout>
+  //               <div>Login</div>
+  //             </AuthLayout>
+  //           }
+  //         />
 
-        <Route
-          path="*"
-          element={
-            <DefaultLayout>
-              <NotFound />
-            </DefaultLayout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>
+  //         <Route
+  //           path="*"
+  //           element={
+  //             <DefaultLayout>
+  //               <NotFound />
+  //             </DefaultLayout>
+  //           }
+  //         />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </ThemeProvider>
 );
 
 export default App;
